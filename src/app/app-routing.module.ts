516b059c8a9bb9df89d84abcaaed63c967a4dc26
villasurlamer <loic.dekester@gmail.com>
2020-10-26 12:54:31 -0700

    setup header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { SignUpComponent } from "./auth/sign-up/sign-up.component";
+import { SignInComponent } from "./auth/sign-in/sign-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/profile', component: ProfileComponent },
+  { path: '/sign-up', component: SignUpComponent },
+  { path: '/sign-in', component: SignInComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
