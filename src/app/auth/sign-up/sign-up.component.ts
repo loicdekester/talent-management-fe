01e13ffa6748628018a8a760a92bce129b6a3a39
villasurlamer <loic.dekester@gmail.com>
2020-10-26 16:44:17 -0700

    validators

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -7,17 +7,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  public signUpForm;
+  public signUpForm: FormGroup;
 
   constructor() {
+  }
+
+  get email() { return this.signUpForm.get('email'); }
+  get password() { return this.signUpForm.get('password'); }
+
+
+  ngOnInit(): void {
     this.signUpForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\?!@#\$%\^&\*]).{8,20}$/)]),
       passwordBis: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\?!@#\$%\^&\*]).{8,20}$/)])
     });
-  }
-
-  ngOnInit(): void {
   }
 
   onSubmit() {
